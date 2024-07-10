e08c4a5a1b509a3726df0b02df564cb8db4ecb80
DenisM <den83178@gmail.com>
2024-07-10 20:19:13 +0300

    Create class ReviewTaskScheduler Methods: GetScheduledTasks, QueueTask, TryDequeue

--- 001_Task_Sheduler/ReviewTaskScheduler.cs
+++ 001_Task_Sheduler/ReviewTaskScheduler.cs
@@ -15,26 +15,51 @@ namespace _001_Task_Sheduler
         }
         protected override void QueueTask(Task task)
         {
-            Console.WriteLine($"             [QueueTask] Task #{task.Id} entered in queue");
+            Console.WriteLine($"             [QueueTask] Task #{task.Id} entered in the queue");
             tasksList.AddLast(task);
             ThreadPool.QueueUserWorkItem(ExecuteTasks, null);
             // ExecuteTasks(null);
         }
 
+        protected override bool TryDequeue(Task task)
+        {
+            Console.WriteLine($"      [TryDequeue] attempt delete Task {task.Id}");
+            bool result = false;
 
-        private void ExecuteTask(object _)
+            lock (tasksList)
+            {
+                result = tasksList.Remove(task);
+            }
+
+            if (result == true)
+            {
+                Console.WriteLine($"   [TryDequeue] Task {task.Id} was deleted from the queue");
+            }
+            return result;
+        }
+
+
+        private void ExecuteTasks(object _)
         {
             while (true)
             {
+                //Thread.Sleep(2000);     // Delete comments = TryExecuteInLine
                 Task task = null;
 
                 lock (tasksList)
                 {
                     if (tasksList.Count == null)
                     {
-
+                        break;
                     }
+                    task = tasksList.First.Value;
+                    tasksList.RemoveFirst();
                 }
+                if(task == null)
+                {
+                    break;
+                }
+                base.TryExecuteTask(task);
             }
         }
     }
