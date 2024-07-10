8428d2edb7bff53a3c716dcc2380bb609d8060df
MSI <MSI@DESKTOP-9SKQCHN>
2024-07-10 19:38:27 +0300

    Add project files.

---
+++ 001_Task_Sheduler/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _001_Task_Sheduler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 001_Task_Sheduler/ReviewTaskScheduler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Task_Sheduler
+{
+    internal class ReviewTaskScheduler : TaskScheduler
+    {
+        private LinkedList<Task> tasksList = new LinkedList<Task>();
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            return tasksList;
+        }
+        protected override void QueueTask(Task task)
+        {
+            Console.WriteLine($"             [QueueTask] Task #{task.Id} entered in queue");
+            tasksList.AddLast(task);
+            ThreadPool.QueueUserWorkItem(ExecuteTasks, null);
+            // ExecuteTasks(null);
+        }
+
+
+        private void ExecuteTask(object _)
+        {
+            while (true)
+            {
+                Task task = null;
+
+                lock (tasksList)
+                {
+                    if (tasksList.Count == null)
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+
+
+
+
+}
